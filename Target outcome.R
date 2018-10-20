8d0db92a26503ef9b6b4ffcd0e5968210e540249
Tanainan C <t.chuanchaiyakul@tilburguniversity.edu>
2018-10-20 23:16:09 +0200

    g

--- Target outcome.R
+++ Target outcome.R
@@ -1,6 +1,6 @@
 #Target Outcome
 
-<<<<<<< HEAD
+
 #Of all the contacted researchers 499 (44%) started the survey. 
 #whereas 291 (26%) respondents finished the survey.
 #One hundred sixty-nine respondents completed the latter version, while 122 respondents completed the former version.
@@ -19,14 +19,6 @@
 #Calculated power (overall) 0.35
 #Calculated power (based on individual answers) 0.40
 #Bias -0.34
-=======
-#A total of 197 respondents answered this open question from a researchers’ perspective 
-#(this includes answers from respondents who did not finish the survey). Two independent raters scored whether the answers could be assigned to
-#one or more of five different categories. The raters agreed in 93% of the cases (Cohen’s kappa = .80). 
-#A power analysis was mentioned by 93 (47%) of the respondents (although 20
-#of them, or 22%, also mentioned practical constraints, like available time and money).
-#Overall, 40 respondents (20%) stated that practical constraints determined their sample size.
-#Furthermore, 45 respondents (23%) mentioned some rule of thumb (e.g., 20 subjects per condition), 41 (21%) based sample sizes on the common practice in their field of research,
-#and 18 respondents (9%) wanted as many participants as possible, to have the highest
-#possible power to detect an effect. 
->>>>>>> e890be8602237f2fe46b5796a666477b2e5e3253
+
+#Considering that 53% of the respondents indicated that they did not generally conduct power analyses and 
+#23% of the respondents used some rule of thumb, we wondered whether respondents’ intuitive power analyses were accurate. 
