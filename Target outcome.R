ba9baa233135def296fb5af1ac8e3dc6b303b7a0
Tanainan C <t.chuanchaiyakul@tilburguniversity.edu>
2018-10-20 14:23:17 +0200

    Target Outcome

---
+++ Target outcome.R
@@ -0,0 +1,11 @@
+#Target Outcome
+
+#A total of 197 respondents answered this open question from a researchers’ perspective 
+#(this includes answers from respondents who did not finish the survey). Two independent raters scored whether the answers could be assigned to
+#one or more of five different categories. The raters agreed in 93% of the cases (Cohen’s kappa = .80). 
+#A power analysis was mentioned by 93 (47%) of the respondents (although 20
+#of them, or 22%, also mentioned practical constraints, like available time and money).
+#Overall, 40 respondents (20%) stated that practical constraints determined their sample size.
+#Furthermore, 45 respondents (23%) mentioned some rule of thumb (e.g., 20 subjects per condition), 41 (21%) based sample sizes on the common practice in their field of research,
+#and 18 respondents (9%) wanted as many participants as possible, to have the highest
+#possible power to detect an effect. 
